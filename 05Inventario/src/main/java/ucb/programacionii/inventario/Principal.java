2f4550d2fdcc95e57f1ea5547b23b2bacb2d8574
Ronal Guerrero <ronal.guerrero@gmail.com>
2024-05-07 17:29:09 -0400

    07/05

--- 05Inventario/src/main/java/ucb/programacionii/inventario/Principal.java
+++ 05Inventario/src/main/java/ucb/programacionii/inventario/Principal.java
@@ -60,6 +60,7 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
         btnEliminar = new javax.swing.JButton();
         btnAgregar = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -93,20 +94,25 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
         jButton1.setText("jButton1");
         jButton1.addActionListener(this);
 
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/demo.jpeg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
+                .addComponent(jScrollPane1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(btnModificar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnEliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(btnAgregar)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnModificar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnEliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(btnAgregar)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addComponent(jLabel1))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -123,6 +129,8 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
                         .addComponent(btnEliminar)
                         .addGap(60, 60, 60)
                         .addComponent(jButton1)
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel1)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addComponent(jScrollPane1))
                 .addContainerGap())
@@ -187,9 +195,9 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/ucb_prog2", "ronal", "Warrior2");
+            //Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/ucb_prog2", "ronal", "Warrior2");
             //Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:FREE", "sys as sysdba", "oracle");
-            //Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/ronal/NetBeansProjects/ucb/05Inventario/data/datos.db");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/ronal/NetBeansProjects/ucb/05Inventario/data/datos.db");
 
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select * from persona");
@@ -198,7 +206,7 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
                 System.out.println("name = " + rs.getString("nombre"));
             }
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -240,6 +248,7 @@ public class Principal extends javax.swing.JFrame implements ActionListener {
     private javax.swing.JButton btnEliminar;
     private javax.swing.JButton btnModificar;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblDatos;
     // End of variables declaration//GEN-END:variables
